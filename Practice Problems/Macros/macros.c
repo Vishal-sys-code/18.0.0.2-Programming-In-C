935401f4fd3555248110209fd89ba921b7e7e2a5
Vishal Pandey <bishalpandey2001@gmail.com>
2022-07-27 16:17:21 +0530

    Create macros.c

---
+++ Practice Problems/Macros/macros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define SUM (n*(n+1)/2)
+// #define variable_name formula/value
+#define ll long long int
+
+ll N; //global variable
+
+int a = 10; //global function
+void main2(){
+    // global function
+    int b = 10; //local variable
+}
+
+// int arr[N]; // global array
+
+int main(){
+    // ll n; // local variable
+    N = 10;
+    // scanf("%d",&n);
+    // int ans = SUM;
+    // printf("%d", SUM);
+    // printf("%d", N);
+    printf("%d", a);
+    return 0;
+}
