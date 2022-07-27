4f023b4e690882a11535c7275b8bbe8dc339915e
Vishal Pandey <bishalpandey2001@gmail.com>
2022-07-27 16:13:14 +0530

    Loops

---
+++ Practice Problems/Loops/12 - GCD_and_LCM_of_two_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+    int a,b,i,gcd;
+    scanf("%d %d", &a,&b);
+    for(i=1; i<=a && i<=b; i++){
+        if(a%i == 0 && b%i == 0){
+            gcd = i;
+        }
+    }
+    printf("GCD: %d\n", gcd);
+
+    int mul = a*b;
+    int lcm;
+    lcm = (a*b)/gcd;
+    printf("LCM: %d", lcm);
+    return 0;
+}
